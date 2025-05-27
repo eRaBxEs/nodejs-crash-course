6126f7a890bf0157bf16e30c078a0461030a763b
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-27 01:44:34 +0100

    directory and file name and path

--- lesson-2/global.js
+++ lesson-2/global.js
@@ -4,11 +4,16 @@
 
 
 // using a settimeout method from the global object
-setTimeout(() => {
-    console.log('in the timeout');
-    clearInterval(int);
-}, 3000); // to run after 3 seconds
+// setTimeout(() => {
+//     console.log('in the timeout');
+//     clearInterval(int);
+// }, 3000); // to run after 3 seconds
 
-const int = setInterval(() => {
-    console.log('in the interval');
-}, 1000); // to run at intervals of 1 second.
+// const int = setInterval(() => {
+//     console.log('in the interval');
+// }, 1000); // to run at intervals of 1 second.
+
+
+
+console.log(__dirname); // get the directory name
+console.log(__filename); // get the filename in fullpath
