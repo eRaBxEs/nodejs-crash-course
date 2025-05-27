31a836e30b38f2995d2b0d0ed4d8c3579d7f6d26
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-27 03:20:42 +0100

    readStream and then pipe as a writeStream

--- lesson-2/streams.js
+++ lesson-2/streams.js
@@ -4,9 +4,12 @@ const readStream = fs.createReadStream('./docs/blog3.txt', { encoding: 'utf-8' }
 const writeStream = fs.createWriteStream('./docs/blog4.txt');
 
 
-readStream.on('data', (chunk) => {
-    console.log('----- NEW CHUNK -----');
-    console.log(chunk);
-    writeStream.write('\nNEW CHUNK\n');
-    writeStream.write(chunk);
-});
+// readStream.on('data', (chunk) => {
+//     console.log('----- NEW CHUNK -----');
+//     console.log(chunk);
+//     writeStream.write('\nNEW CHUNK\n');
+//     writeStream.write(chunk);
+// });
+
+// piping
+readStream.pipeline(writeStream);
