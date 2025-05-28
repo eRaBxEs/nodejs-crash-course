74888e0045cff32f9d3337cc58167ca5b4ce5655
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-28 15:43:58 +0100

    render the ejs pages with render method

--- lesson-4/app.js
+++ lesson-4/app.js
@@ -4,21 +4,22 @@ const express = require('express');
 // express app
 const app = express();
 
+// reister view engines
+app.set('view engine', 'ejs');
+
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
 
-    // it infers the header information
-    // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 
 app.get('/about', (req, res) => {
 
-    // it infers the header information
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 
 });
 
@@ -28,5 +29,5 @@ app.get('/about-us', (req, res) => {
 
 // 404 page
 app.use((req, res) => { // these code should always be at the bottom
-    res.status(404).sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
