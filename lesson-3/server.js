254eb1777be8a5661ddcc6855467bc5c291ab1b9
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-28 13:05:32 +0100

    setting status code for each page

--- lesson-3/server.js
+++ lesson-3/server.js
@@ -6,16 +6,19 @@ const server = http.createServer((req, res) => {
     // set header content type
     res.setHeader('Content-type', 'text/html');
 
-    let path = './views';
+    let path = './views/';
     switch (req.url) {
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
