2f1592f3018f5ca3a8c28577b3c2978050838a7e
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-28 14:07:23 +0100

    simple set up of express js for server

---
+++ lesson-4/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+
+// express app
+const app = express();
+
+// listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+
+    // it infers the header information
+    res.send('<p>home page</p>');
+});
