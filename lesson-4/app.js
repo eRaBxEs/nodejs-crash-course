33a5f7600875d526621147d0f6d772c443f622f4
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-29 21:08:05 +0100

    working on the url path to get all blogs

--- lesson-4/app.js
+++ lesson-4/app.js
@@ -26,54 +26,10 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 
 
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog 2',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    });
-
-    blog.save()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-
-// routes : get all the blogs
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('6838aa7c78278f04c2be430f')
-        .then((result) => {
-            res.send(result);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
-
 
 app.get('/', (req, res) => {
-    const blogs = [
-        { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum sit amet consectetur' },
-        { title: 'mario finds stairs', snippet: 'Lorem ipsum sit amet consectetur' },
-        { title: 'How to defeat bowsor', snippet: 'Lorem ipsum sit amet consectetur' }
-    ]
 
-    res.render('index', { title: 'Home', blogs: blogs });
+    res.redirect('/blogs');
 });
 
 
@@ -82,6 +38,18 @@ app.get('/about', (req, res) => {
     res.render('about', { title: 'About' });
 
 });
+
+
+// blog routes
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({ createdAt: -1 })
+        .then((result) => {
+            res.render('index', { title: 'All Blogs', blogs: result });
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+})
 
 app.get('/blogs/create', (req, res) => {
     res.render('create', { title: 'Create' });
