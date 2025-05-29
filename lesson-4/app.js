8a2ff74424da10920af00baa659fcc2ec40f5a5c
Ehioje Henry Erabor <ehiojee@yahoo.com>
2025-05-29 19:54:06 +0100

    doing some database  queries in mongodb

--- lesson-4/app.js
+++ lesson-4/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
@@ -24,6 +24,47 @@ app.listen(3000);
 app.use(express.static('public'));
 // middleware & request logger
 app.use(morgan('dev'));
+
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'new blog 2',
+        snippet: 'about my new blog',
+        body: 'more about my new blog'
+    });
+
+    blog.save()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
+// routes : get all the blogs
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('6838aa7c78278f04c2be430f')
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
+
 
 app.get('/', (req, res) => {
     const blogs = [
